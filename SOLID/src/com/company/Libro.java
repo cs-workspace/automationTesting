370eb8f99ad58873d686a7a4227ee458a23a1ab2
steven-n-wilson <49218333+steven-n-wilson@users.noreply.github.com>
2020-09-04 16:09:10 -0600

    created file and added single responsibility example

---
+++ SOLID/src/com/company/Libro.java
@@ -0,0 +1,28 @@
+package com.company;
+
+/*
+Automation Testing
+Entrega: 9/5/2020
+@steven_wilson
+*/
+
+// Single Responsability
+
+public class Libro {
+
+    public String getTitulo() {
+        return "Mi Libro";
+    }
+
+    public String getAutor() {
+        return "Steven Wilson";
+    }
+
+    public String printPagina() {
+        return "Contenido actual de la pagina";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
