2a420fe8c79c1e7ffa8c6314a5dce470cb9a952b
steven-n-wilson <49218333+steven-n-wilson@users.noreply.github.com>
2020-09-25 07:37:03 -0600

    added testing examples

--- CalculadoraTesting/src/test/java/parameters/TestWithParameters.java
+++ CalculadoraTesting/src/test/java/parameters/TestWithParameters.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TestWithParameters {
-    @Test(groups = "")
+    @Test(groups = {"parameters", "all"})
     @Parameters({"FirstParameter", "SecondParameter"})
     public void testCaseWithParameters(String param1, String param2){
         System.out.println(param1 + "" + param2);
